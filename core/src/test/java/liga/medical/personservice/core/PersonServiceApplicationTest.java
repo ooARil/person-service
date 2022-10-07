c1811b869667cd155013c22aa90174b8c41cacde
arseniy ilyinsky <ilinskiyar@gmail.com>
2022-10-08 01:55:41 +0300

    Add person-service

---
+++ core/src/test/java/liga/medical/personservice/core/PersonServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package liga.medical.personservice.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PersonServiceApplicationTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
